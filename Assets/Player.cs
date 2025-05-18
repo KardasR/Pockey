3a7570a632b48e198a80182b2f9929871c49cc3a
KardasR <38445317+KardasR@users.noreply.github.com>
2025-05-18 01:18:05 -0400

    Added comments, improved some logic

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,61 +2,97 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    #region Fields
+    #region Serialized Fields
 
+    [Header("Movement Settings")]
+    
+    /// <summary>
+    /// Base movement speed.
+    /// </summary>
     [SerializeField]
     private float skatingSpeed;
 
-    [SerializeField]
-    private float shootingSpeed;
-
+    /// <summary>
+    /// Multiplier applied to the skating speed when sprinting.
+    /// </summary>
     [SerializeField]
     private float sprintModifier;
 
+    [Header("Shooting Settings")]
+
+    /// <summary>
+    /// Speed the puck is shoot.
+    /// </summary>
+    [SerializeField]
+    private float shootingSpeed;
+    
+    [Header("References")]
+
+    /// <summary>
+    /// Where the puck is held when picked up.
+    /// </summary>
     [SerializeField]
     private Transform puckHoldPoint;
 
+    #endregion Serialized Fields
+
+    #region Private Fields
+
+    /// <summary>
+    /// Rigidbody2D component of the player.
+    /// </summary>
     private Rigidbody2D body;
 
+    /// <summary>
+    /// The direction the player is moving.
+    /// </summary>
     private Vector2 skatingDirection;
 
+    /// <summary>
+    /// Is the player currently sprinting?
+    /// </summary>
     private bool isSprinting;
 
+    /// <summary>
+    /// Reference to the held puck if there is one.
+    /// </summary>
     private Puck heldPuck;
 
-    #endregion Fields
+    #endregion Private Fields
 
     #region Private Methods
 
+    /// <summary>
+    /// Movement method for the player.
+    /// </summary>
     private void Skate()
     {
         body.linearVelocity = skatingDirection * (isSprinting ? skatingSpeed * sprintModifier : skatingSpeed);
     }
 
+    /// <summary>
+    /// Method to shoot the currently held puck in the same direction as the player.
+    /// </summary>
     private void ShootPuck()
     {
-        heldPuck.Release(skatingDirection * shootingSpeed);
+        heldPuck.Release(shootingSpeed * (skatingDirection == Vector2.zero ? Vector2.left : skatingDirection));
         heldPuck = null;
-    }
-
-    private void MovePuck()
-    {
-        if (heldPuck != null)
-        {
-            heldPuck.transform.position = puckHoldPoint.position;
-        }
     }
 
     #endregion Private Methods
 
     #region Public Methods
 
+    /// <summary>
+    /// Try to pickup a puck when one is encountered.
+    /// </summary>
+    /// <param name="puck"></param>
     public void TryPickupPuck(Puck puck)
     {
         if (heldPuck == null && puck.IsPickupable)
         {
             heldPuck = puck;
-            puck.Hold();
+            puck.Hold(puckHoldPoint);
         }
     }
 
@@ -75,20 +111,16 @@ public class Player : MonoBehaviour
     {
         skatingDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-            isSprinting = true;
-
-        if (Input.GetKeyUp(KeyCode.LeftShift))
-            isSprinting = false;
+        isSprinting = Input.GetKey(KeyCode.LeftShift);
 
         if (Input.GetKeyDown(KeyCode.Space) && heldPuck != null)
             ShootPuck();
     }
 
+    // Called at fixed intervals for physics updates
     void FixedUpdate()
     {
         Skate();
-        MovePuck();
     }
 
     #endregion Core Unity Methods
--- Assets/Puck.cs
+++ Assets/Puck.cs
@@ -1,47 +1,72 @@
 using System;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Puck : MonoBehaviour
 {
     #region Fields
 
+    /// <summary>
+    /// How long to wait until allowing the puck to be picked up.
+    /// </summary>
     [SerializeField]
     private float pickupCooldownDuration;
 
+    /// <summary>
+    /// Time left before cooldown time is over.
+    /// </summary>
     private float pickupCooldownTimeleft;
 
+    /// <summary>
+    /// Rigidbody2D component of the puck.
+    /// </summary>
     private Rigidbody2D body;
 
+    /// <summary>
+    /// Is the puck currently held?
+    /// </summary>
     private bool isHeld = false;
+    
+    /// <summary>
+    /// Can the puck be picked up?
+    /// <para/>
+    /// Can't be held or have time left on the cooldown timer.
+    /// </summary>
+    public bool IsPickupable => !isHeld && pickupCooldownTimeleft <= 0f;
 
     #endregion Fields
 
-    #region Public Properties
-
-    public bool IsPickupable
-    {
-        get
-        {
-            return !isHeld && pickupCooldownTimeleft <= 0f;
-        }
-    }
-
-    #endregion Public Properties
-
     #region Public Methods
 
-    public void Hold()
+    /// <summary>
+    /// Turn off physics so the puck can be carried easier. Mark the puck is held.
+    /// </summary>
+    public void Hold(Transform holdPoint)
     {
         isHeld = true;
+
+        // stop physics
         body.simulated = false;
+
+        // Attach to the puck hold point
+        transform.SetParent(holdPoint);
+        transform.localPosition = Vector3.zero;
     }
 
+    /// <summary>
+    /// Shoot the puck with the given force. Mark the puck is not held.
+    /// </summary>
+    /// <param name="force">Force to impulse push the puck with.</param>
     public void Release(Vector2 force)
     {
         pickupCooldownTimeleft = pickupCooldownDuration;
 
         isHeld = false;
+        transform.SetParent(null);
+
+        // turn physics back on
         body.simulated = true;
+        body.linearVelocity = Vector2.zero; // avoid stacking forces
         body.AddForce(force, ForceMode2D.Impulse);
     }
 
@@ -49,6 +74,7 @@ public class Puck : MonoBehaviour
 
     #region Core Unity Methods
 
+    //
     void Awake()
     {
         body = GetComponent<Rigidbody2D>();
